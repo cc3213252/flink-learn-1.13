aa4614af8ab194c9baa1d40ffcfe1bb4c06943c9
yudan.chen <cc3213252@163.com>
2022-05-03 17:08:35 +0800

    p87 p88 统计TopN的两种方法

--- wordcount/src/main/java/com/blueegg/chapter07/EventTimeTest.java
+++ wordcount/src/main/java/com/blueegg/chapter07/EventTimeTest.java
@@ -8,6 +8,7 @@ import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.util.Collector;
 
 import java.sql.Timestamp;
@@ -18,7 +19,7 @@ public class EventTimeTest {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
 
-        SingleOutputStreamOperator<Event> stream = env.addSource(new ClickSource())
+        SingleOutputStreamOperator<Event> stream = env.addSource(new CustomSource())
                 .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ZERO)
                         .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
                             @Override
@@ -40,10 +41,29 @@ public class EventTimeTest {
 
                     @Override
                     public void onTimer(long timestamp, OnTimerContext ctx, Collector<String> out) throws Exception {
-                        out.collect(ctx.getCurrentKey() + "定时器触发，触发时间： " + new Timestamp(timestamp));
+                        out.collect(ctx.getCurrentKey() + "定时器触发，触发时间： " + new Timestamp(timestamp) + " watermark: " + ctx.timerService().currentWatermark());
                     }
                 }).print();
 
         env.execute();
+    }
+
+    // 自定义测试数据源
+    public static class CustomSource implements SourceFunction<Event>{
+        @Override
+        public void run(SourceContext<Event> ctx) throws Exception {
+            // 直接发出测试数据
+            ctx.collect(new Event("Mary", "./home", 1000L));
+            Thread.sleep(5000L);
+            ctx.collect(new Event("Alice", "./home", 11000L));
+            Thread.sleep(5000L);
+            ctx.collect(new Event("Bob", "./home", 11001L));
+            Thread.sleep(5000L);
+        }
+
+        @Override
+        public void cancel() {
+
+        }
     }
 }
