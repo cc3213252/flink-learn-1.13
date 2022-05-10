3384968c7a18db7b7f4248d99f81deacae0a1c69
yudan.chen <cc3213252@163.com>
2022-05-11 05:03:59 +0800

    p132 使用TableEnvironment输出到csv

--- doc/tableapi.md
+++ doc/tableapi.md
@@ -1,3 +1,7 @@
+## dashboard
+
+使用TableEnvironment输出到csv的一个例子：  CommonApiTest
+
 ## 引入包
 
 flink-table-api-java-bridge_${scala.binary.version}  负责table api和下层datastream api的连接支持  
@@ -9,3 +13,13 @@ flink-table-api-java-bridge_${scala.binary.version}  负责table api和下层dat
 result> +I[Mary, ./cart]   I, insert  
 eventTable.select($("user"), $("url"))  $表达式引入  
 SimpleTableExample， table api和sql两种方式入门，较好  
+
+## 创建表的方式
+
+连接器表  
+虚拟表
+
+## 碰到问题
+
+Could not find any format factory for identifier 'csv' in the classpath  
+需要引入flink-csv依赖  
--- wordcount/src/main/java/com/blueegg/chapter11/CommonApiTest.java
+++ wordcount/src/main/java/com/blueegg/chapter11/CommonApiTest.java
@@ -3,9 +3,12 @@ package com.blueegg.chapter11;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.bridge.java.BatchTableEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+import static org.apache.flink.table.api.Expressions.$;
 
 public class CommonApiTest {
     public static void main(String[] args) throws Exception {
@@ -24,23 +27,58 @@ public class CommonApiTest {
 
         TableEnvironment tableEnv = TableEnvironment.create(settings);
 
-        // 1.1 基于老版本planner进行流处理
-        EnvironmentSettings settings1 = EnvironmentSettings.newInstance()
-                .inStreamingMode()
-                .useOldPlanner()
-                .build();
+//        // 1.1 基于老版本planner进行流处理
+//        EnvironmentSettings settings1 = EnvironmentSettings.newInstance()
+//                .inStreamingMode()
+//                .useOldPlanner()
+//                .build();
+//
+//        TableEnvironment tableEnv1 = TableEnvironment.create(settings1);
+//
+//        // 1.2 基于老版本planner进行批处理
+//        ExecutionEnvironment batchEnv = ExecutionEnvironment.getExecutionEnvironment();
+//        BatchTableEnvironment batchTableEnv = BatchTableEnvironment.create(batchEnv);
+//
+//        // 1.3 基于blink版本planner进行批处理
+//        EnvironmentSettings settings3 = EnvironmentSettings.newInstance()
+//                .inBatchMode()
+//                .useBlinkPlanner()
+//                .build();
+//        TableEnvironment tableEnv3 = TableEnvironment.create(settings3);
 
-        TableEnvironment tableEnv1 = TableEnvironment.create(settings1);
+        // 2. 创建表（连接器表）
+        String createDDL = "CREATE TABLE clickTable (" +
+                " user_name STRING, " +
+                " url STRING, " +
+                " ts BIGINT " +
+                ") WITH (" +
+                " 'connector' = 'filesystem', " +
+                " 'path' = 'input/clicks.txt'," +
+                " 'format' = 'csv'" +
+                ")";
 
-        // 1.2 基于老版本planner进行批处理
-        ExecutionEnvironment batchEnv = ExecutionEnvironment.getExecutionEnvironment();
-        BatchTableEnvironment batchTableEnv = BatchTableEnvironment.create(batchEnv);
+        tableEnv.executeSql(createDDL);
+        // 调用table api进行表的查询转换
+        Table clickTable = tableEnv.from("clickTable");
+        Table resultTable = clickTable.where($("user_name").isEqual("Bob"))
+                .select($("user_name"), $("url"));
+//        tableEnv.createTemporaryView("result", resultTable);
 
-        // 1.3 基于blink版本planner进行批处理
-        EnvironmentSettings settings3 = EnvironmentSettings.newInstance()
-                .inBatchMode()
-                .useBlinkPlanner()
-                .build();
-        TableEnvironment tableEnv3 = TableEnvironment.create(settings3);
+        // 执行sql进行表的查询转换
+//        Table resultTable2 = tableEnv.sqlQuery("select user_name from result");
+
+        // 创建一张用于输出的表
+        String createOutDDL = "CREATE TABLE outTable (" +
+                " url STRING, " +
+                " user_name STRING " +
+                ") WITH (" +
+                " 'connector' = 'filesystem', " +
+                " 'path' = 'output'," +
+                " 'format' = 'csv'" +
+                ")";
+        tableEnv.executeSql(createOutDDL);
+
+        // 输出表
+        resultTable.executeInsert("outTable");
     }
 }
